65bede495c6ca87f36493fa24d6b4ec228ff4d46
shamily1994 <shamilysasidharan111@gmail.com>
2021-12-31 22:26:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -61,6 +61,10 @@ app.get('/',function(req,res){
 
 
 //part#1 point5
-app.listen(3000,()=>{
-    console.log("Server Ready on 3000");
-});
+// app.listen(3000,()=>{
+//     console.log("Server Ready on 3000");
+// });
+//part#1 point5
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
